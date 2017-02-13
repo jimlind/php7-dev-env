fcec5c31ef8baf8088a4a8a7000d7fc3791adf61
Jim Lind <spoon.vw@gmail.com>
2017-02-12 23:14:44 -0500

    Force modern simple push

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -12,6 +12,7 @@ bash "git-user-config" do
   environment ({'HOME' => '/home/vagrant'})
   cwd "#{node['project_dir']}"
   code <<-END
+    git config --global push.default simple
     git config --global user.name #{node['git']['user_name']};
     git config --global user.email #{node['git']['user_email']};
     git clone #{node['git']['repository_url']} #{node['git']['repository_dir']};
